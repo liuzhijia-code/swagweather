e6db5045c72a5b0f4211c9acb1dfd03c7e28b448
Jia <614366617@qq.com>
2018-10-04 09:19:09 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/example/swagweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.swagweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String contyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContyName() {
+        return contyName;
+    }
+
+    public void setContyName(String contyName) {
+        this.contyName = contyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
